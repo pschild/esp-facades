62e1bd4f34ae1245f6632189754e6d2b3a47d2f1
Philippe Schild <pschild@users.noreply.github.com>
2020-06-09 19:35:30 +0200

    Update MyLib.h

--- src/MyLib.h
+++ src/MyLib.h
@@ -1,1 +1,20 @@
+#ifndef MyLib_h
+#define MyLib_h
 
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+
+class MyLib {
+  public:
+    MyLib(char* brokerIP, char* clientName, WiFiClient wifiClient);
+    void setup();
+    void loop();
+    void reconnect();
+  private:
+    char* _brokerIP;
+    char* _clientName;
+    PubSubClient* _mqttClient;
+};
+
+#endif
