5d0cb145157b8bd86fa540b9f2ce3757f35ca7c7
Philippe Schild <pschild@users.noreply.github.com>
2020-06-16 21:46:27 +0200

    Create OTAUpdateHandler.h

---
+++ src/OTAUpdateHandler.h
@@ -0,0 +1,23 @@
+#ifndef OTAUpdateHandler_h
+#define OTAUpdateHandler_h
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+class OTAUpdateHandler {
+  public:
+    OTAUpdateHandler(String updateServerIP, String version);
+    void setup();
+    void loop();
+    void checkForUpdate();
+    void startUpdate();
+    void setInterval(long interval);
+  private:
+    String _updateServerIP;
+    String _version;
+    long _updateInterval;
+    long _lastUpdateCheck;
+    String buildEnpointUrl();
+};
+
+#endif
